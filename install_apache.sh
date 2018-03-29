82e6dbfc4e232989991040b11589ed4b82f6d531
amolmore3171 <amol.more8084@gmail.com>
2018-03-29 14:33:26 -0500

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -5,6 +5,6 @@ apt-get -y update
 apt-get -y install r-base
 apt-get -y install apache2
 apt-get -y install gdebi-core
-wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
-gdebi rstudio-server-1.1.383-amd64.deb
-EXPOSE 80 443
+#wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
+#gdebi rstudio-server-1.1.383-amd64.deb
+#EXPOSE 80 443

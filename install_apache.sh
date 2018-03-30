5fb2b5cde58612101e08463558dee3fe3f254623
amolmore3171 <amol.more8084@gmail.com>
2018-03-29 23:15:35 -0500

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -1,10 +1,6 @@
 apt-get -y update
 apt-get -y upgrade
-#add-apt-repository ppa:marutter/rrutter
-apt-get -y update
 apt-get -y install r-base-core
 apt-get -y install apache2
-#apt-get -y install gdebi-core
-#wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
-#gdebi rstudio-server-1.1.383-amd64.deb
-#EXPOSE 80 443
+
+apt-get -y update

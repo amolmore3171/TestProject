0d28b9d3c2c3705ec42e89bae691024c421ae0a3
amolmore3171 <amol.more8084@gmail.com>
2018-04-16 18:10:41 -0500

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -1,10 +1,11 @@
-apt-get -y update
-apt-get -y upgrade
-add-apt-repository ppa:marutter/rrutter
-apt-get -y update
-apt-get -y install r-base
-apt-get -y install apache2
-apt-get -y install gdebi-core
-wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
-gdebi  rstudio-server-1.1.383-amd64.deb
-EXPOSE 80 443
+apt-get update && apt-get install -y software-properties-common
+add-apt-repository "deb http://cran.r-project.org/bin/linux/ubuntu $(lsb_release -cs)/"
+apt-get update && apt-get install -y sudo 
+apt-get update && apt-get install -y wget
+apt-get update && apt-get install -y r-base
+apt-get update && apt-get install -y r-base-dev
+apt-get update && apt-get install -y r-recommended
+apt-get update && apt-get install -y gdebi-core 
+wget --no-verbose "https://download2.rstudio.org/rstudio-server-1.1.442-amd64.deb" -O ss-latest.deb && \
+gdebi -n ss-latest.deb
+EXPOSE 8787

6bc53a0828b0a037c3e11602201c4464fdd0e2de
amolmore3171 <amol.more8084@gmail.com>
2018-04-16 18:16:12 -0500

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -6,6 +6,5 @@ apt-get update && apt-get install -y r-base
 apt-get update && apt-get install -y r-base-dev
 apt-get update && apt-get install -y r-recommended
 apt-get update && apt-get install -y gdebi-core 
-wget --no-verbose "https://download2.rstudio.org/rstudio-server-1.1.442-amd64.deb" -O ss-latest.deb && \
-gdebi -n ss-latest.deb
+wget --no-verbose "https://download2.rstudio.org/rstudio-server-1.1.442-amd64.deb" -O ss-latest.deb && gdebi -n ss-latest.deb
 EXPOSE 8787

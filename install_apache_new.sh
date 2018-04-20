859fefbec182c36921dbeef5ff54c72ab60f5b31
amolmore3171 <amol.more8084@gmail.com>
2018-04-20 11:55:45 -0500

    Update install_apache_new.sh

--- install_apache_new.sh
+++ install_apache_new.sh
@@ -18,3 +18,6 @@ systemctl enable docker
 apt-get -y update
 apt-get -y install cifs-utils
 apt-get -y install nfs-common
+
+mkdir /mnt/dataikufs
+mount -t cifs //testdataikufs0001.file.core.windows.net/test /mnt/dataikufs -o vers=3.0,username=testdataikufs0001,password=idmzqB3NnKcdAZAgfGK9abSMJwiRumln7Yzc4dTR79p6AgxbuY/SPR9QVOm9miuu8Hx/vKumgUo9Ukfes+fvvw==,dir_mode=0777,file_mode=0777,sec=ntlmssp

b776fc6d3756a45e0416497142ead90076e1b7db
amolmore3171 <amol.more8084@gmail.com>
2018-04-20 11:23:10 -0500

    Update install_apache_new.sh

--- install_apache_new.sh
+++ install_apache_new.sh
@@ -9,3 +9,12 @@ echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.h
 
 # restart Apache
 apachectl restart
+
+apt-get -y install docker.io
+
+systemctl start docker
+systemctl enable docker
+
+apt-get -y update
+apt-get -y install cifs-utils
+apt-get -y install nfs-common

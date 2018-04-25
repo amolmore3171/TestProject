3482504a147f8557133ad534aa98ed8be60362f7
amolmore3171 <amol.more8084@gmail.com>
2018-04-25 12:28:13 -0500

    Update install_apache_new.sh

--- install_apache_new.sh
+++ install_apache_new.sh
@@ -2,13 +2,13 @@
 apt-get -y update
 
 # install Apache2
-apt-get -y install apache2 
+#apt-get -y install apache2 
 
 # write some HTML
-echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+#echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
 
 # restart Apache
-apachectl restart
+#apachectl restart
 
 apt-get -y install docker.io
 
@@ -19,8 +19,8 @@ apt-get -y update
 apt-get -y install cifs-utils
 apt-get -y install nfs-common
 
-mkdir -p /mnt/dataikufs
-sudo mount -t cifs //tststorageacc02.file.core.windows.net/myshare /mnt/dataikufs -o vers=3.0,username=tststorageacc02,password=nDD+FdyoyiJ+e27Hbz01dBqNbELoalKr8beCr8S9p3AZ/c3cULCvldFgXSNDYzBi9oOwp2P96CijHxUfZLoKbQ==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+#mkdir -p /mnt/dataikufs
+#sudo mount -t cifs //tststorageacc02.file.core.windows.net/myshare /mnt/dataikufs -o vers=3.0,username=tststorageacc02,password=nDD+FdyoyiJ+e27Hbz01dBqNbELoalKr8beCr8S9p3AZ/c3cULCvldFgXSNDYzBi9oOwp2P96CijHxUfZLoKbQ==,dir_mode=0777,file_mode=0777,sec=ntlmssp
 
-docker login testdockerreg.azurecr.io --username testdockerreg --password =g5NGo162CJhlkk9tIaoBOlABy0KIkTu
-docker pull testdockerreg.azurecr.io/dataikuimage:latest
+#docker login testdockerreg.azurecr.io --username testdockerreg --password =g5NGo162CJhlkk9tIaoBOlABy0KIkTu
+#docker pull testdockerreg.azurecr.io/dataikuimage:latest

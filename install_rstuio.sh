756c3be214a89a88a80be9d9760b5ba55bca8673
amolmore3171 <amol.more8084@gmail.com>
2018-04-26 14:58:38 -0500

    Update install_rstuio.sh

--- install_rstuio.sh
+++ install_rstuio.sh
@@ -6,29 +6,20 @@ cd /data01
 sudo wget https://download2.rstudio.org/rstudio-server-pro-1.1.423-amd64.deb
 sudo echo "y" | gdebi rstudio-server-pro-1.1.423-amd64.deb
 
-apt-get install openjdk-8*
-apt-get install readline*
-apt-get install xorg-dev
-apt-get install libcurl4-openssl-dev
-apt-get install libpango*
-apt-get install libcairo*
+apt-get -y install openjdk-8*
+apt-get -y install readline*
+apt-get -y install xorg-dev
+apt-get -y install libcurl4-openssl-dev
+apt-get -y install libpango*
+apt-get -y install libcairo*
 
 mkdir -p /opt/R/3.3.1
-mkdir -p /opt/R/3.4.1
 
 cd /tmp
 wget https://cran.r-project.org/src/base/R-3/R-3.3.1.tar.gz
 tar -xvzf R-3.3.1.tar.gz
 cd /tmp/R-3.3.1
 ./configure --prefix=/opt/R/3.3.1 --enable-R-shlib
-make
-make install
-
-cd /tmp
-wget https://cran.r-project.org/src/base/R-3/R-3.4.1.tar.gz
-tar -zxvf R-3.4.1.tar.gz
-cd /tmp/R-3.4.1
-./configure --prefix=/opt/R/3.4.1 --enable-R-shlib
 make
 make install
 

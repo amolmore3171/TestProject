5a51b3fcd2260f31b8297be4dde983c09a7920cd
amolmore3171 <amol.more8084@gmail.com>
2018-04-26 13:52:25 -0500

    Update install_rstuio.sh

--- install_rstuio.sh
+++ install_rstuio.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 apt-get -y update
 sudo apt-get -y install r-base gdebi-core libapparmor1
+mkdir /data01
+cd /data01
 sudo wget https://download2.rstudio.org/rstudio-server-pro-1.1.423-amd64.deb
-sudo gdebi ./var/lib/waagent/custom-script/download/0/rstudio-server-pro-1.1.423-amd64.deb
+sudo gdebi rstudio-server-pro-1.1.423-amd64.deb
 
 #apt-get -y update
 #apt-get -y upgrade

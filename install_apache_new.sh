7adc0688abdb91fd5b068d2101d7cb7fd5aaac09
amolmore3171 <amol.more8084@gmail.com>
2018-04-26 00:02:33 -0500

    Update install_apache_new.sh

--- install_apache_new.sh
+++ install_apache_new.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 apt-get -y update
-
-# install Apache2
-#apt-get -y install apache2 
-
-# write some HTML
-#echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
-
-# restart Apache
-#apachectl restart
-
 apt-get -y install docker.io
-
 systemctl start docker
 systemctl enable docker
 
@@ -19,8 +8,10 @@ apt-get -y update
 apt-get -y install cifs-utils
 apt-get -y install nfs-common
 
-#mkdir -p /mnt/dataikufs
-#sudo mount -t cifs //tststorageacc02.file.core.windows.net/myshare /mnt/dataikufs -o vers=3.0,username=tststorageacc02,password=nDD+FdyoyiJ+e27Hbz01dBqNbELoalKr8beCr8S9p3AZ/c3cULCvldFgXSNDYzBi9oOwp2P96CijHxUfZLoKbQ==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+mkdir /users01 /projects01
+
+'echo "//storacct01platformpred.file.core.windows.net/users01 /users01 cifs nofail,vers=3.0,username=storacct01platformpred,password=xHncT7It2RicG5GQdvPuwIaq5DN0SMPiiYNkJEsgLAg1QmgBS7kfniTs6R0hjNRGr9FnjCY1vRA82J4BwUni3g==,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
+'echo "//storacct01platformpred.file.core.windows.net/projects01 /projects01 cifs nofail,vers=3.0,username=storacct01platformpred,password=xHncT7It2RicG5GQdvPuwIaq5DN0SMPiiYNkJEsgLAg1QmgBS7kfniTs6R0hjNRGr9FnjCY1vRA82J4BwUni3g==,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
 
 docker login conacreg01platformpred.azurecr.io --username conacreg01platformpred --password wxaACwC0zIpi8/uCXbJKnv/mQkjcI50i
 docker pull conacreg01platformpred.azurecr.io/dataikuimg_v4.2.2:latest
